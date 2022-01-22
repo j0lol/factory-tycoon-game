1fde7dcdd3b9a528110d61ae76286d373bec078a
BuildTools <unconfigured@null.spigotmc.org>
2022-01-22 01:39:46 +0000

    i just zoned out for an hour

--- InputManager.gd
+++ InputManager.gd
@@ -5,7 +5,7 @@ extends Node2D
 # var a = 2
 # var b = "text"
 var mouse_collide = false
-
+var mouse_collide_big = false
 
 onready var hotbar = get_node("/root/Root/CanvasLayer/Hotbar")
 onready var map = get_node("/root/Root/TileMap")
@@ -19,8 +19,8 @@ func _process(delta):
 	input()
 	match hotbar.selected:
 		1: tile_input()
-		2: object_input("res://Machine.tscn","Machine")
-		3: object_input("res://Chest.tscn","Chest")
+		2: object_input("res://Machine.tscn","Machine", true)
+		3: object_input("res://Chest.tscn","Chest", false)
 
 
 func input(): 
@@ -44,13 +44,19 @@ func tile_input():
 		place_tile_mouse(-1)
 
 
-func object_input(link, name):
-	if Input.is_action_just_pressed("click"):
-		if !mouse_collide:
-			spawn_scene(link,name)
-	elif Input.is_action_pressed("rclick"):
-		spawn_scene("res://Killer.tscn","Killer")
-
+func object_input(link, name, big):
+	if big:
+		if Input.is_action_pressed("click"):
+			if !mouse_collide_big:
+				spawn_scene(link,name)
+		elif Input.is_action_pressed("rclick"):
+				spawn_scene("res://Killer.tscn","Killer")
+	else:
+		if Input.is_action_pressed("click"):
+			if !mouse_collide:
+				spawn_scene(link,name)
+		elif Input.is_action_pressed("rclick"):
+				spawn_scene("res://Killer.tscn","Killer")
 
 
 func place_tile_mouse(tile):
@@ -60,8 +66,6 @@ func place_tile_mouse(tile):
 		map.rotations[global.rotation][1], 
 		map.rotations[global.rotation][2])
 
-
-
 func _physics_process(delta):
 	query_space()
 	
@@ -69,17 +73,26 @@ func query_space():
 	var physics := get_world_2d().direct_space_state
 
 	var hits = physics.intersect_ray(get_viewport().get_mouse_position(), 
-				get_viewport().get_mouse_position(), [],
+				get_viewport().get_mouse_position() + Vector2(0,1), [],
 				0x7FFFFFFF, false, true)
 	if hits:
 		mouse_collide = true
+		mouse_collide_big = true
 	else:
 		mouse_collide = false
-
-
+	
+	var hits1 = physics.intersect_ray(get_viewport().get_mouse_position() + Vector2(0,16), 
+			get_viewport().get_mouse_position() + Vector2(16,16), [],
+			0x7FFFFFFF, false, true)
+	var hits2 = physics.intersect_ray(get_viewport().get_mouse_position(), 
+			get_viewport().get_mouse_position() + Vector2(16,0), [],
+			0x7FFFFFFF, false, true)
+	if hits1 or hits2:
+		mouse_collide_big = true
+	else:
+		mouse_collide_big = false
 
 func spawn_scene(link, name):
-	print("PLACE")
 	var s = load(link).instance()
 	s.name = name
 	s.position = get_viewport().get_mouse_position() - Vector2(8,8)
